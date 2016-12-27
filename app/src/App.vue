37f57cab0a3da8cef80a46f3403712ee3cb01d2b
Jaggy Gauran <i.am@jag.gy>
2016-12-27 12:27:31 +0800

    Follow linting.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,40 +1,27 @@
-<style>
-  @import url(https://fonts.googleapis.com/css?family=Lato:300);
-
-  * {
-    margin: 0;
-    padding: 0;
-  }
-
-  html,
-  body { height: 100%; }
-
-  body {
-    align-items: center;
-    background:
-      radial-gradient(
-        ellipse at center,
-        rgba(255, 255, 255, 1) 0%,
-        rgba(229, 229, 229, .85) 100%
-      );
-    background-position: center;
-    display: flex;
-    font-family: Lato, Helvetica, sans-serif;
-    justify-content: center;
-    text-align: center;
-  }
-</style>
-
 <template>
   <div>
+    <tree-view :project="project"></tree-view>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import store from 'src/vuex/store'
+import Vue from 'vue'
+import store from 'src/vuex/store'
+import Project from 
 
-  export default {
-    store
+Vue.component('tree-view', require('src/components/TreeView.vue'))
+
+export default {
+  store,
+
+  data () {
+    return {
+      project: {
+        name: 'test'
+      }
+    }
   }
+}
 </script>
--- app/src/components/TreeView.vue
+++ app/src/components/TreeView.vue
@@ -1,49 +1,49 @@
 <template>
-    <aside class="tree-view">
-        <nav class="project">
-            <h2 class="tree-view__header project__name">
-                <a href="#">{{ project.name }}</a>
-            </h2>
+  <aside class="tree-view">
+    <nav class="project">
+      <h2 class="tree-view__header project__name">
+        <a href="#">{{ project.name }}</a>
+      </h2>
 
-            <ul class="project__nodes node-list">
-                <li class="node-item"
-                    v-for="node in project.nodes">
-                    <a class="node-item__link" href="#"
-                       :class="[ 
-                           icon(node),
-                           { 'icon-folder': node.is_directory }
-                       ]"
-                       @click="select(node)">
-                        <label class="node-item__name">{{ node.name }}</label>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </aside>
+      <ul class="project__nodes node-list">
+        <li class="node-item"
+          v-for="node in project.nodes">
+          <a class="node-item__link" href="#"
+             :class="[ 
+               icon(node),
+               { 'icon-folder': node.is_directory }
+             ]"
+             @click="select(node)">
+            <label class="node-item__name">{{ node.name }}</label>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </aside>
 </template>
 
 <script>
-    export default {
-        props: {
-            project: Object
-        },
+  export default {
+    props: {
+      project: Object
+    },
 
-        methods: {
-            icon (node) {
-                if (! node.filetype) {
-                    return null;
-                }
-
-                return `icon-${node.filetype}`;
-            },
-
-            select (node) {
-                if (node.is_directory) {
-                    return console.error(`expand directory ${node.path}`);
-                }
-
-                console.error(`opening file ${node.path}`);
-            }
+    methods: {
+      icon (node) {
+        if (!node.filetype) {
+          return null
         }
+
+        return `icon-${node.filetype}`
+      },
+
+      select (node) {
+        if (node.is_directory) {
+          return console.error(`expand directory ${node.path}`)
+        }
+
+        console.error(`opening file ${node.path}`)
+      }
     }
+  }
 </script>
